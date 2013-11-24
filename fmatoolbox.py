a914ca44367eb6cd98b933db6d238038af7cb178
Frédéric MARTIN <frederic.martin.fma@gmail.com>
2013-11-24 02:02:47 +0100

    fix config file generation

--- fmatoolbox.py
+++ fmatoolbox.py
@@ -93,6 +93,7 @@ class Config(object):
 
 		self.sections = OrderedDict()
 		self._default_section = self.add_section(Section("DEFAULT"))
+		self.parser = None
 
 	def add_section(self, section):
 		if not isinstance(section, Section):
@@ -135,9 +136,6 @@ class Config(object):
 			s.load(self.fileParser)
 		log.debug("configuration loaded.")
 
-	def write_default_config_file(self):
-		pass
-
 	def reload(self, args):
 		log.debug("reloading configuration ...")
 		if args.config_file :
@@ -146,16 +144,6 @@ class Config(object):
 			log.debug("loading section : " + s.name)
 			s.load(self.fileParser)
 		log.debug("configuration reloaded.")
-
-#	def reload(self, args):
-#		if args.server_section or args.config :
-#			server_section_name = self.section
-#			if args.server_section :
-#				server_section_name += '-' + str(args.server_section)
-#
-#			myPref = MyPref(self.filepath, server_section_name, expanduser=True, prefix = "\t", mandatory = False)
-#			self.linshare = myPref(self.fields)
-#			self.decode_password()
 
 	def __getattr__(self, name):
 		if name.lower() == "default":
@@ -168,9 +156,9 @@ class Config(object):
 						% { "name" : name, "class" : self.__class__.__name__ } )
 
 	def get_parser(self , **kwargs):
-		parser = argparse.ArgumentParser( prog = self.prog_name , description=self.desc, **kwargs)
-		parser.add_argument('-c', '--config-file',	action="store", help="Other configuration file.")
-		return parser
+		self.parser = argparse.ArgumentParser( prog = self.prog_name , description=self.desc, **kwargs)
+		self.parser.add_argument('-c', '--config-file',	action="store", help="Other configuration file.")
+		return self.parser
 
 	def __str__(self):
 		res = []
@@ -179,20 +167,19 @@ class Config(object):
 			res.append("".join(s.get_representation("\t")))
 		return "\n".join(res)
 
+	def write_default_config_file(self , output , comments = True):
+		with open(output, 'w') as f:
+			if comments :
+				f.write("#####################################\n")
+				f.write("Description :\n")
+				f.write("-------------\n")
+				f.write(self.desc)
+				f.write("\n\n")
 
-#	def write_default_config_file(self):
-#
-#		log = logging.getLogger('linshare-cli')
-#		filepath = os.path.expanduser(self.filepath)
-#		if os.path.isfile(filepath) :
-#			log.error("Can not generate the pref file because it already exists. : " + filepath)
-#			return 1
-#		else :
-#			with open(filepath, 'w') as f:
-#				f.write("[" + self.section + "]\n")
-#				for field in self.fields:
-#					f.write(field + "=\n")
-#
+			for s in self.sections.values():
+				log.debug("loading section : " + s.name)
+				s.write_config_file(f , comments)
+		log.info("config file generation complete : " + str(output))
 
 # ---------------------------------------------------------------------------------------------------------------------
 class Section(object):
@@ -254,6 +241,24 @@ class Section(object):
 	def __str__(self):
 		return "".join(self.get_representation())
 
+	def write_config_file(self , f , comments):
+		if len(self.elements) < 1 :
+			return
+
+		if comments :
+			f.write("#####################################\n")
+			f.write("# Section : " + self.name + "\n")
+			f.write("#####################################\n")
+		f.write("[" + self.name + "]\n")
+		if self.desc and comments:
+			f.write("# Description : ")
+			f.write(self.desc)
+			f.write("\n")
+
+		for e in self.elements.values() :
+			e.write_config_file(f , comments)
+		f.write("\n")
+
 # ---------------------------------------------------------------------------------------------------------------------
 class ListSection(object):
 
@@ -261,13 +266,13 @@ class ListSection(object):
 		pass
 # ---------------------------------------------------------------------------------------------------------------------
 class Element(object):
-	def __init__(self, name, e_type = str, required = False, default = None, required_as_arg = False, desc = None, hooks = [ DefaultHook() ], hidden = False ):
+	def __init__(self, name, e_type = str, required = False, default = None, conf_hidden = False, desc = None, hooks = [ DefaultHook() ], hidden = False ):
 		self.name = name
 		self.e_type = e_type
 		self.required = required
 		self.default = default
-		self.required_as_arg = required_as_arg
 		self.desc = desc
+		self.conf_hidden = conf_hidden
 		self.desc_for_config = None
 		self.desc_for_argparse = None
 		self.value = None
@@ -341,8 +346,8 @@ class Element(object):
 
 	def get_arg_parse_arguments(self):
 		ret = dict()
-		if self.required_as_arg :
-			ret["required"] = self.required_as_arg
+		if self.required :
+			ret["required"] = self.required
 		if self.name :
 			ret["dest"] = self.name
 		if self.value :
@@ -350,6 +355,31 @@ class Element(object):
 		if self.desc :
 			ret["help"] = self.desc
 		return ret
+
+	def write_config_file(self , f, comments):
+		if self.conf_hidden:
+			return False
+
+		if comments:
+			f.write("\n")
+			f.write("# Attribute (")
+			f.write(str(self.e_type.__name__))
+			f.write(") : ")
+			f.write(self.name.upper())
+			f.write("\n")
+			if self.desc and self.desc != argparse.SUPPRESS:
+				f.write("# Description : ")
+				f.write(self.desc)
+				f.write("\n")
+
+
+		if not self.required:
+			f.write(";")
+		f.write(self.name)
+		f.write("=")
+		if self.default:
+			f.write(self.default)
+		f.write("\n")
 
 # ---------------------------------------------------------------------------------------------------------------------
 class DefaultCompleter(object):
@@ -382,3 +412,97 @@ class DefaultCompleter(object):
 
 		except Exception as e:
 			debug("\nERROR:An exception was caught :" + str(e) + "\n")
+# ---------------------------------------------------------------------------------------------------------------------
+class SampleProgram(object):
+
+	def __init__(self , parser , config):
+		self.parser = parser
+		self.config = config
+
+	def __call__(self):
+		log = logging.getLogger()
+		if not self.parser :
+			log.error("Parser attribute is not set. Call get_parser() method or create your own parser.")
+			return False
+
+		# integration with argcomplete python module (bash completion)
+		try:
+			import argcomplete
+			argcomplete.autocomplete(self.parser)
+		except ImportError as e :
+			pass
+
+		# parse cli arguments
+		args = self.parser.parse_args()
+
+		if getattr(args, 'debug'):
+			log.setLevel(logging.DEBUG)
+			streamHandler.setFormatter(myDebugFormat)
+			print "------------- config ------------------"
+			print self.config
+			print "----------- processing ----------------"
+
+			# run command
+			args.__func__(args)
+			return True
+		else:
+			try:
+				# run command
+				args.__func__(args)
+				return True
+			except ValueError as a :
+				log.error("ValueError : " + str(a))
+			except KeyboardInterrupt as a :
+				log.warn("Keyboard interruption detected.")
+			except Exception as a :
+				log.error("unexcepted error : " + str(a))
+			return False
+
+# ---------------------------------------------------------------------------------------------------------------------
+#
+#
+def query_yes_no(question, default="yes"):
+	res = _query_yes_no(question, default)
+	if res == "yes":
+		return True
+	else:
+		return False
+
+# ---------------------------------------------------------------------------------------------------------------------
+#
+#
+def _query_yes_no(question, default="yes"):
+	"""Ask a yes/no question via raw_input() and return their answer.
+
+	"question" is a string that is presented to the user.
+	"default" is the presumed answer if the user just hits <Enter>.
+		It must be "yes" (the default), "no" or None (meaning
+		an answer is required of the user).
+
+	The "answer" return value is one of "yes" or "no".
+	"""
+	valid = {"yes":"yes",   "y":"yes",  "ye":"yes",
+			 "no":"no",     "n":"no"}
+	if default == None:
+		prompt = " [y/n] "
+	elif default == "yes":
+		prompt = " [Y/n] "
+	elif default == "no":
+		prompt = " [y/N] "
+	else:
+		raise ValueError("invalid default answer: '%s'" % default)
+
+	while 1:
+		sys.stdout.write(question + prompt)
+		try:
+			choice = raw_input().lower()
+		except KeyboardInterrupt as e :
+			print
+			return "no"
+		if default is not None and choice == '':
+			return default
+		elif choice in valid.keys():
+			return valid[choice]
+		else:
+			sys.stdout.write("Please respond with 'yes' or 'no' "\
+			                 "(or 'y' or 'n').\n")
