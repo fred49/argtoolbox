93861c9d7fd1e49e3586561cb84039ce5ab9ff70
Frédéric MARTIN <frederic.martin.fma@gmail.com>
2014-04-04 01:28:01 +0200

    fix files headers (license header was missing)

--- samples/sampleprogram.py
+++ samples/sampleprogram.py
@@ -2,6 +2,28 @@
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
 
+# This file is part of argtoolbox.
+#
+# argtoolbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# argtoolbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with LinShare user cli.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2014 Frédéric MARTIN
+#
+# Contributors list:
+#
+#  Frédéric MARTIN frederic.martin.fma@gmail.com
+#
+
 from argtoolbox import DefaultCommand
 from argtoolbox import BasicProgram
 
